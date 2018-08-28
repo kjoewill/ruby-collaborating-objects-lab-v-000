8fbd19f35e69e0ef678d7ea7a34545f6d131baae
Kevin W <kjoewill@gmail.com>
2018-08-28 05:21:22 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ class MP3Importer
     @path = path
     full_path = self.path + "/*.mp3"
     filenames_with_path = Dir[full_path]
-    @files = filenames_with_path
+    @files = filenames_with_path.collect { |e| short_name(e) }
   end
 
   def files
